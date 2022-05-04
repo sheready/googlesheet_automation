132ca08650b82bba18142d3d9147e68d00660b8e
sheready <csstlw@gmail.com>
2022-05-04 09:08:42 +0300

    validating users credentials

--- load_googlesheet/auth.py
+++ load_googlesheet/auth.py
@@ -14,3 +14,19 @@ SAMPLE_SPREADSHEET_ID = '1HkUSpRdMtYKXI3RxNz9tz5jz1Pa5HpuS'
 SAMPLE_RANGE_NAME = 'Employee data!A1:J'
 
 creds = None
+#The file token.json stores the users access and refresh tokens,it's created
+#automatically when the authosization flow completes for the first time
+if os.path.exists('token.json'):
+    creds = Credentials.from_authorized_user_file('token.json',SCOPES)
+#if there are no (valid) credentials available, let the user log in
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json',SCOPES
+        )
+        creds = flow.run_local_server(port=0)
+        #Saving the credentials for the next run
+        with open('token.json','w') as token:
+            token.write(creds.to_json())
