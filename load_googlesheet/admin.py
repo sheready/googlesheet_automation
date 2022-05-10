0c48c360c15197688416037fe5b57203c98aad92
sheready <csstlw@gmail.com>
2022-05-10 23:25:23 +0300

    automating google sheet contents to dictionaries

--- load_googlesheet/admin.py
+++ load_googlesheet/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+#configuring the admin console in django to insert records manually
+#registering created model class to admin console
+script = """ from django.contrib import admin
+from .models import * 
+"""
+#Register your models here.
 
-# Register your models here.
+tables = {}
+for model in tables.keys():
+    script = script + "admin.site.register(" + model + ")\n"
+script
+
+#saving the file
+root = 'load_googlesheet/'
+file_name = root + 'admin.py'
+
+with open(file_name, "w", encoding='utf-8') as py_file:
+    py_file.write(script)
--- load_googlesheet/auth.py
+++ load_googlesheet/auth.py
@@ -1,5 +1,8 @@
-from __future__ import print_function
 import os.path
+import pandas as pd
+
+from __future__ import print_function
+
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -30,3 +33,74 @@ if not creds or not creds.valid:
         #Saving the credentials for the next run
         with open('token.json','w') as token:
             token.write(creds.to_json())
+service = build('sheets', 'v4', credentials=creds)
+
+#Call the sheets API
+sheet = service.spreadsheets()
+
+#loading the content of the google sheet and converting spreadsheet into dict of dataframes
+
+sheet_metadata = service.spreadsheets().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME).execute()
+
+#transforming the content into a dictionary of pandas dataframe
+df_dict = {}
+properties = sheet_metadata.get('sheets')
+
+for item in properties:
+    table = item.get('properties').get('title')
+    df_dict[table] = pd.DataFrame()
+
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=table + '!A1:J').execute()
+    header = result.get('values',[])[0]
+    values = result.get('values',[])[1:]
+    if not values:
+        print('No values found')
+    else:
+        all_data = []
+        for col_id, col_name in enumerate(header):
+            column_data = []
+            for row in values:
+                if col_id < len(row):
+                    column_data.append(row[col_id])
+                else:
+                    column_data.append('')
+            ds = pd.Series(data=column_data, name=col_name)
+            all_data.append(ds)
+        df_dict[table] = pd.concat(all_data, axis=1)
+
+#creating a mapping between the tables and django model class
+
+tables = {}
+for table,df in df_dict.items():
+    tables[table] = {}
+    for i in range(0, df.shape[0]):
+        attr = {}
+        if df['MAX LENGTH'][i] != '':
+            attr['max_length'] = df['MAX LENGTH'][i]
+        if df['KEY'][i] == 'primary key':
+            attr['primary_key'] = 'True'
+        tables[table][df['ATTRIBUTES'][i]] = [df['DATA TYPE'][i],attr]
+tables
+
+#create the content of the model.py script
+def get_type(attr_type):
+    if isinstance(attr_type, list):
+        attr = attr_type[0] + 'Field('
+        for k,v in attr_type[1].items():
+            attr = attr + k + '=' + v + ','
+        attr = attr[:-1]
+        return attr + (')\n')
+    else:
+        return attr_type + 'field()\n'
+
+script = 'from django.db import models\n'
+#build model class for each item in the table
+for model,attributes in tables.items():
+    script = script + "class" + model + "(models.Model):\n"
+    for attr_name,attr_type in attributes.items():
+        script = script + '\t' + attr_name + '=models.' + get_type(attr_type)
+#save result in models.py
+root = 'core/load_googlesheet/'
+file_name = root + 'models.py'
+with open(file_name, "w") as py_file:
+    py_file.write(script)
